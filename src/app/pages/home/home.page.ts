ffd319f823e940fc1b104bb3a1543fdad04b01bd
Juan Medina <soyjuanmedina@gmail.com>
2024-05-12 18:16:42 +0200

    'New_front'

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,6 @@
 import { trigger, transition, query, style, stagger, animate, keyframes } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
-import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Params, Router, RouterModule, RouterOutlet } from '@angular/router';
 import { UserService } from '../../services/user.service';
 import { UtilitiesService } from '../../services/utilities.service';
 import { CommonModule } from '@angular/common';
@@ -15,8 +15,31 @@ declare let $: any;
 })
 export class HomePage implements OnInit {
 
+  confirmMailMessageSuccess: string;
+  confirmMailMessageDanger: string;
+  uuid: string;
 
-  constructor(public _userService: UserService, public _utilitiesService: UtilitiesService, public router: Router) {
+  constructor (private activatedRoute: ActivatedRoute, public _userService: UserService, public _utilitiesService: UtilitiesService, public router: Router) {
+    this.activatedRoute.queryParams.subscribe((params: Params) => {
+      if (params['uuid']) {
+        this.uuid=params['uuid']
+        this.confirmMail(this.uuid);
+      }
+    });
+  }
+
+  confirmMail(uuid: string) {
+    let params={
+      uuid: uuid
+    }
+    this._userService.confirmEmail(params).subscribe(data => {
+      this.confirmMailMessageSuccess='Gracias por confirmar tu mail. Ahora ya puedes loguearte en Culturaxya';
+      this._utilitiesService.loading=false;
+    },
+      err => {
+        this.confirmMailMessageDanger='No existe el usuario'
+        this._utilitiesService.loading=false;
+      });
   }
 
 
